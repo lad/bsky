45c327d31ff7a5c26d46aa90075054501078fe54
lad <1081096+lad@users.noreply.github.com>
2025-04-11 15:20:56 +0100

    Minor formatting changes

--- test/test_get_unread_notification_count.py
+++ test/test_get_unread_notification_count.py
@@ -11,10 +11,11 @@ from partial_failure import PartialFailure
 
 class TestGetUnreadNotificationCount(BaseTest):
     '''Test BlueSky get_unread_notifications_count() method'''
-    @pytest.mark.parametrize('count', range(1,11))
+    @pytest.mark.parametrize('count', range(1, 11))
     def test_get_unread_notification_count(self, count):
+        '''Test unread notification count'''
         self.instance.client.app.bsky.notification.get_unread_count.return_value = \
-                count
+            count
 
         assert self.instance.get_unread_notifications_count() == count
 
@@ -27,7 +28,7 @@ class TestGetUnreadNotificationCount(BaseTest):
             self.instance.get_unread_notifications_count()
         assert guc.call_count == self.instance.FAILURE_LIMIT
 
-    @pytest.mark.parametrize('count', range(1,11))
+    @pytest.mark.parametrize('count', range(1, 11))
     def test_get_unread_notification_count_with_retries(self, count):
         '''Test get_unread_notifications_count_cmd() method with exceptions partial
            failure causing retries'''
